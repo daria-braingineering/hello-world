33fe385883eefac2594e4bc0e5be5f90a61d8ffc
dariapohasii <daria@simbly.ai>
2025-10-21 15:50:17 +0200

    test commit 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center px-4 py-16">
-        <h1 className="text-5xl font-bold tracking-tight text-green-300 sm:text-[5rem] mb-4">
+        <h1 className="text-5xl font-bold tracking-tight text-yellow-300 sm:text-[5rem] mb-4">
           Hello world!
         </h1>
         <h2 className="text-3xl font-sm mb-2">
